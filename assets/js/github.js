0248bbe756c64d5f493a095e88c773f124ba97a3
Cole French <cfrench2019@columbiasecondary.org>
2016-06-08 03:45:49 +0000

    Slightly change github link

--- assets/js/github.js
+++ assets/js/github.js
@@ -2,7 +2,7 @@ var pathToRepo = "https://github.com/howtoamerica/howtoamerica.github.io";
 var target;
 
 $('a[href$="#github"]').click(function() {
-    target = pathToRepo + "/blob/master" + window.location.href.substring(window.location.protocol.length + pathToRepo.substring(pathToRepo.lastIndexOf("/") - 1).length);
+    target = pathToRepo + "/blob/master" + window.location.href.substring(window.location.protocol.length + pathToRepo.substring(pathToRepo.lastIndexOf("/") - 1).length).replace("#github", "");
     
     if (window.location.href.charAt(window.location.href.length - 1) == "/") {
         target = target.concat("index");
